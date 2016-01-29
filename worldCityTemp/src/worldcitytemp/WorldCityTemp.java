42031e8ea827879da783f8de4d3a21d51ca7cf88
Corey Acri <acri.corey@gmail.com>
2016-01-28 20:59:14 -0500

    latest code for assignment 2

--- worldCityTemp/src/worldcitytemp/WorldCityTemp.java
+++ worldCityTemp/src/worldcitytemp/WorldCityTemp.java
@@ -4,14 +4,14 @@
  * Date: Jan. 28, 2016
  * Description: This program:
  * - asks user for the name of a city
- * - asks for the current temp in degrees Celsius 
- * - converts the user input in Celsius to Fahrenheit 
- * - prints the results in this format: 
+ * - asks for the current temp in degrees Celsius
+ * - converts the user input in Celsius to Fahrenheit
+ * - prints the results in this format:
  *   "The current temperature in London is 20 0C, which is 68 0F"
- * 
+ *
  */
-
 package worldcitytemp;
+
 import java.util.*; // import java util 
 
 public class WorldCityTemp {
@@ -20,17 +20,35 @@ public class WorldCityTemp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         
-        // declare variables
-        
+
+    // declare variables
         // city name
-        String cityName; 
-        
+        String cityName;
+
         // temperature in Celsius input from user
-        double tempCelsius; 
+        double tempCelsius;
         
-       // temperature in Fahrenheit
+        // temperature in Fahrenheit
+        double tempFahrenheit;
         
+        // multiplier for Celsius
+        double celsiusMultiplier = 9/5;
+        System.out.println(celsiusMultiplier);
+        
+        // degree normalizer to add remainging degrees to temp conversion
+        double degreeNormalizer;
+
+        
+
+        // declare an instance of Scanner to read the data stream from the keyboard.
+        Scanner kb = new Scanner(System.in);
+
+        // retrieve city name from user 
+        System.out.println("Hello, please enter your city: ");
+        cityName = kb.nextLine();
+        
+        
+
     }
-    
+
 }
