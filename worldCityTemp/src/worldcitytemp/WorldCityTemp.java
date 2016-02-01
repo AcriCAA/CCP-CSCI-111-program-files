208f6877e7661486ef4264cab7f4e6eee8e6bad7
Corey Acri <acri.corey@gmail.com>
2016-01-31 21:44:49 -0500

    adding WorldCityTemp

--- worldCityTemp/src/worldcitytemp/WorldCityTemp.java
+++ worldCityTemp/src/worldcitytemp/WorldCityTemp.java
@@ -10,9 +10,10 @@
  *   "The current temperature in London is 20 0C, which is 68 0F"
  *
  */
+
 package worldcitytemp;
 
-import java.util.*; // import java util 
+import java.util.*; // import java util libraries
 
 public class WorldCityTemp {
 
@@ -22,33 +23,62 @@ public class WorldCityTemp {
     public static void main(String[] args) {
 
     // declare variables
+        
         // city name
         String cityName;
 
-        // temperature in Celsius input from user
-        double tempCelsius;
+        // temperature in Celsius for input from user
+        double degreesCelsius;
         
-        // temperature in Fahrenheit
-        double tempFahrenheit;
+        //temperature in Fahrenheit after conversion from Celsius
+        double degreesFahrenheit;
         
         // multiplier for Celsius
-        double celsiusMultiplier = 9/5;
-        System.out.println(celsiusMultiplier);
+        double celsiusMultiplier = 9.0/5.0;
         
-        // degree normalizer to add remainging degrees to temp conversion
-        double degreeNormalizer;
-
+        // degree normalizer to add remaining degrees to temp conversion
+        double degreeNormalizer = 32;
         
-
         // declare an instance of Scanner to read the data stream from the keyboard.
         Scanner kb = new Scanner(System.in);
 
+     // get input from user. 
+        
         // retrieve city name from user 
         System.out.println("Hello, please enter your city: ");
         cityName = kb.nextLine();
         
+        // Asks for the current temp in degrees Celsius requiring a number
+        System.out.println("\n" + cityName + ", great! \n\nPlease tell me the "
+                + "current temperature in " + cityName + " in degrees Celsius: ");
         
+         do
+            { 
+                try {
+                    String s = kb.nextLine();
+                    degreesCelsius = Double.parseDouble(s);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    System.out.println("\nPlease try again, entering only a number:");
+                }
+            }
+            while (true);
 
-    }
+        //converts the user provided the temperature in Celsius to Fahrenheit  
+        degreesFahrenheit = (celsiusMultiplier * degreesCelsius) + 
+                             degreeNormalizer;
+        
+        // tells user the current temperature in degrees Fahrenheit
+        
+        System.out.println("\nThe current temperature in " + cityName + " is " + 
+                degreesCelsius + "\u00b0" + " C, which is " + degreesFahrenheit +
+                        "\u00b0" + " F.");
+        
+        // close the input stream
+        kb.close();
+        
+    } // end main method
 
-}
+}  // end WorldCityTemp class
