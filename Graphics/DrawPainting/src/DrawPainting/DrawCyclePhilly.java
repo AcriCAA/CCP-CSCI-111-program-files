6cf89d88cf43e6f8331f4f2cfb60ab76fe10eb1b
Corey Acri <acri.corey@gmail.com>
2016-04-16 21:56:36 -0400

    BOOOOOOOM CyclePhilly logo written with Java graphics

--- Graphics/DrawPainting/src/DrawPainting/DrawCyclePhilly.java
+++ Graphics/DrawPainting/src/DrawPainting/DrawCyclePhilly.java
@@ -18,7 +18,7 @@ public class DrawCyclePhilly {
         // set up a JFrame to hold the canvas
         JFrame frame = new JFrame();
         frame.setTitle("Using Common Graphics Class Methods");
-        frame.setSize(700,431);
+        frame.setSize(821,821);
         frame.setLocation(100, 100);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -37,16 +37,13 @@ class MyJPanel extends JPanel {
     public void paint(Graphics graphics) {
     
         
-        graphics.drawRect(0, 0, 700, 431);
+        graphics.drawRect(0, 0, 821, 821);
         graphics.drawOval(140, 20, 100, 100);
-//        graphics.drawRoundRect(260, 20, 100, 200, 20, 20);
-//        graphics.drawArc(340, 20, 100, 100, 0, 90);
-//        graphics.drawLine(20, 240, 400, 280);
-                
+
 // draw the background
         
         graphics.setColor(new Color(255,255,255)); //white bg
-        graphics.fillRect(0, 0, 700, 431);
+        graphics.fillRect(0, 0, 821,821);
 
 
 // draw top chevron 
@@ -74,163 +71,107 @@ class MyJPanel extends JPanel {
         int nBottomChevron = 3; 
                 // left bottom, top, right bottom
         int[] xBottomChevron = {180,400,630};
-        int[] yBottomChevron = {150,80,150};
-        graphics.fillPolygon(xBottomChevron,yBottomChevron,nBottomChevron);        
+        int[] yBottomChevron = {200,120,200};
+        graphics.fillPolygon(xBottomChevron,yBottomChevron,nBottomChevron); 
         
         
+// draw bottom white chevron to cover part of bottom chevron
+
+        graphics.setColor(Color.white); 
+        int nWhiteChevron2 = 3; 
+                // left bottom, top, right bottom
+        int[] xWhiteChevron2 = {180,400,630};
+        int[] yWhiteChevron2 = {220,140,220};
+        graphics.fillPolygon(xWhiteChevron2,yWhiteChevron2,nWhiteChevron2);        
+        
+ // draw a black bg just for formatting and positioning for now
+ 
+//      graphics.setColor(Color.gray);
+//      graphics.fillRect(181,221,450, 450);      
+      
+// draw handlebars 
+
+    graphics.setColor(new Color(42,108,173)); //cyclephilly blue
+    graphics.fillRect(295,221,85,15);      
+    
+// draw seat
+
+    graphics.setColor(new Color(42,108,173)); //cyclephilly blue
+    graphics.fillRect(440,250,70,16);      
+
+// draw left wheel exterior
+
+      graphics.setColor(new Color(42,108,173)); //cyclephilly blue
+      graphics.fillOval(181,350,175,175);   
+      
+      
+      graphics.setColor(Color.white); //white
+      graphics.fillOval(190,360,155,155);   
+      
+
+      
+// draw right wheel exterior
+
+      graphics.setColor(new Color(42,108,173)); //cyclephilly blue
+      graphics.fillOval(450,350,175,175);      
+     
+      graphics.setColor(Color.white); //white
+      graphics.fillOval(461,360,155,155);  
+      
+      
+// draw bike frame parralellogram 
+        
+        graphics.setColor(new Color(42,108,173)); //cyclephilly blue
+        int nCenterFrame = 4; 
+               
+        int[] xCenterFrame = {310,490,565,390};
+        int[] yCenterFrame = {315,315,440,440};
+        graphics.fillPolygon(xCenterFrame,yCenterFrame,nCenterFrame);  
+        
+// draw left triagle in frame        
+
+        graphics.setColor(Color.white); 
+        int nCenterFrameTriangle = 3; 
+                // left bottom, top, right bottom
+        int[] xCenterFrameTriangle = {330,390,460};
+        int[] yCenterFrameTriangle = {325,425,325};
+        graphics.fillPolygon(xCenterFrameTriangle,yCenterFrameTriangle,nCenterFrameTriangle);  
+        
+// draw right triagle in frame        
+
+        graphics.setColor(Color.white); 
+        int nCenterFrameTriangle1 = 3; 
+                // left bottom, top, right bottom
+        int[] xCenterFrameTriangle1 = {400,475,545};
+        int[] yCenterFrameTriangle1 = {430,325,430};
+        graphics.fillPolygon(xCenterFrameTriangle1,yCenterFrameTriangle1,nCenterFrameTriangle1); 
+        
+// draw seat tube
+
+        
+        graphics.setColor(new Color(42,108,173)); //cyclephilly blue
+        int nSeatTube = 4; 
+                // left top, left bottom, right top, right bottom
+        int[] xSeatTube = {520,500,380,400};
+        int[] ySeatTube = {250,250,425,425};
+        graphics.fillPolygon(xSeatTube,ySeatTube,nSeatTube);  
+        
+
+// fork
+        
+        graphics.setColor(new Color(42,108,173)); //cyclephilly blue
+        int nFork = 4; 
+                // left top, left bottom, right top, right bottom
+        int[] xFork = {355,345,258,270};
+        int[] yFork = {235,235,430,430};
+        graphics.fillPolygon(xFork,yFork,nFork);  
+        
+        graphics.setColor(new Color(42,108,173)); //cyclephilly blue
+        graphics.setFont(new Font("Sans",Font.BOLD,100)); 
+        graphics.drawString("CyclePhilly",150,650);
         
 
 
-//        
-//        graphics.setColor(new Color(33,167,201)); //turquoise
-//        graphics.fillRect(225, 80, 30, 30);
-        
-//// draw a middle window
-//        
-//        graphics.setColor(Color.gray);
-//        graphics.fillRect(300, 170, 85, 30);        
-//
-//// draw a right window
-//        
-//        graphics.setColor(Color.gray);
-//        graphics.fillRect(325, 170, 30, 30); 
-//        
-//// draw a door
-//        
-//        graphics.setColor(Color.RED);
-//        graphics.fillRect(315, 275, 50, 75);         
-//        
-//// draw the chiminey *****************************
-//
-//        graphics.setColor(Color.RED);
-//        graphics.fillRect(350, 85, 25, 50);
-//                //        x, y, width, height
-//        graphics.setColor(Color.GREEN);
-//        
-//// end chiminey **********************************    
-//
-//
-//
-//
-//// a moon 
-//    
-//    graphics.setColor(Color.white);
-//    graphics.fillOval(120, 30, 100, 100);    
-//    
-//// cover part of the a moon 
-//    
-//    graphics.setColor(Color.black);
-//    graphics.fillOval(145, 25, 100, 100);  
-//    
-//// a star 
-//    
-//    graphics.setColor(Color.yellow);
-//    graphics.fillOval(75, 30, 10, 10);       
-//
-//// a star 
-//    
-//    graphics.setColor(Color.blue);
-//    graphics.fillOval(10, 40, 5, 5);    
-//    
-//
-//// a star 
-//    
-//    graphics.setColor(Color.yellow);
-//    graphics.fillOval(350, 45, 5, 5);      
-//    
-//// a star 
-//    
-//    graphics.setColor(Color.white);
-//    graphics.fillOval(400, 49, 5, 5);     
-//    
-////  shooting stars 
-//
-//int xStar = 450; 
-//        
-//int yStar = 20;
-//int count = 0; 
-//int width = 5; 
-//int height = 5; 
-//while (count <= 10){
-//
-//    graphics.fillOval(xStar,yStar, width, height);     
-//    xStar+=5;
-//    yStar+=11;
-//    width++;
-//    height++;
-//    count++;
-//
-//}
-//
-//        
-//    
-//       
-//
-//// a tree (left) ****************************************        
-//        
-//        //tree top
-//        graphics.setColor(Color.GREEN);
-//        int n = 3; 
-//                // left bottom, top, right bottom
-//        int[] x = {30,42,60};
-//        int[] y = {300,200,300};
-//        graphics.fillPolygon(x,y,n);
-//        
-//        
-//        // tree trunk
-//        graphics.setColor(new Color(92,9,9));
-//        graphics.fillRect(40, 301, 10, 50);
-//        
-//// end tree **************************************   
-//
-//
-//// a tree (left) ****************************************        
-//        
-//        //tree top
-//        graphics.setColor(Color.GREEN);
-//        int nRightTree = 3; 
-//                // left bottom, top, right bottom
-//        int[] xRightTree = {500,512, 530};
-//        int[] yRightTree = {300,200,300};
-//        graphics.fillPolygon(xRightTree,yRightTree,nRightTree);
-//        
-//        
-//        // tree trunk
-//        graphics.setColor(new Color(92,9,9));
-//        graphics.fillRect(510, 301, 10, 50);
-//        
-//// end tree **************************************   
-//
-//
-        
-
-        // out line the filled red square with a black square
-//        graphics.setColor(Color.black);
-//        graphics.drawRect(20, 300, 100, 100);
-
-//        graphics.setColor(Color.green);
-//        graphics.fillOval(140, 300, 100, 200);
-        
-//        graphics.setColor(Color.blue);
-//        graphics.fillRoundRect(260, 300, 100, 200, 60, 60);
-        
-//        graphics.setColor(new Color(128,0,128));
-//        graphics.fillArc(340, 340, 100, 100, 0, 90);
-        
-        // draw a parallelogram
-//        int n = 4;
-//        int[] x = {20, 100, 140, 60};
-//        int[] y = {420, 420, 480, 480};
-//        graphics.drawPolygon(x, y, n);
-
-        // draw some text
-//        graphics.setColor(Color.black);
-//        graphics.setFont(new Font("Times New Roman", Font.BOLD, 12));
-//        graphics.drawString("Hello World!", 390, 150);
-
-        // add a logo to the canvas
-//        Image logo = new ImageIcon("logo.jpg").getImage();
-//        graphics.drawImage(logo, 390, 440, null);
 
     }  // end paint()
 } // end class MyCanvas
